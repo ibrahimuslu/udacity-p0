6ab34d0406f258dd9b3c096dbedf690be95f3d59
Ibrahim USLU <ibrahimuslu@gmail.com>
2020-02-01 00:55:18 +0300

    Task 2 done

--- Task2.py
+++ Task2.py
@@ -10,7 +10,23 @@ with open('texts.csv', 'r') as f:
 with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
-
+    phoneDict = {}
+    for call in calls:
+        if call[0] not in phoneDict:
+            phoneDict[call[0]]= int(call[3])
+        else:
+            phoneDict[call[0]]+=int(call[3])
+        if call[1] not in phoneDict:
+            phoneDict[call[1]]=int(call[3])
+        else:
+            phoneDict[call[1]]+=int(call[3])
+    longestDuration = 0
+    thePhoneNumber = ''
+    for phone in phoneDict:
+        if phoneDict[phone]>longestDuration:
+            longestDuration = phoneDict[phone]
+            thePhoneNumber = phone
+    print(thePhoneNumber," spent the longest time, ",longestDuration," seconds, on the phone during September 2016.")
 """
 TASK 2: Which telephone number spent the longest time on the phone
 during the period? Don't forget that time spent answering a call is
