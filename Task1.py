21376b968d280abbe333dc31c52054e73144e5de
Ibrahim USLU <ibrahimuslu@gmail.com>
2020-02-02 12:57:28 +0300

    Fixed for recurring numbers in texts and calls

--- Task1.py
+++ Task1.py
@@ -4,10 +4,11 @@ It's ok if you don't understand how to read files.
 """
 import csv
 count =0
+phoneDict = {}
 with open('texts.csv', 'r') as f:
     reader = csv.reader(f)
     texts = list(reader)
-    phoneDict = {}
+    
     for text in texts:
         if text[0] not in phoneDict:
             phoneDict[text[0]]=1
@@ -22,8 +23,6 @@ with open('texts.csv', 'r') as f:
 with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
-
-    phoneDict = {}
     for call in calls:
         if call[0] not in phoneDict:
             phoneDict[call[0]]=1
@@ -33,9 +32,9 @@ with open('calls.csv', 'r') as f:
             phoneDict[call[1]]=1
         else:
             phoneDict[call[1]]+=1
-    for phone in phoneDict:
-        count+=1
-    print("There are {} different telephone numbers in the records.".format(count))
+for phone in phoneDict:
+    count+=1
+print("There are {} different telephone numbers in the records.".format(count))
 
 """
 TASK 1:
